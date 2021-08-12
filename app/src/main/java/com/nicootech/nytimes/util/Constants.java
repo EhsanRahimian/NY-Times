ad3869b1d597903ccad91e9ee7089ae69106061e
Ethan Rahimian <nicootech@gmail.com>
2021-08-12 15:40:21 -0700

    Constants

---
+++ app/src/main/java/com/nicootech/nytimes/util/Constants.java
@@ -0,0 +1,6 @@
+package com.nicootech.nytimes.util;
+
+public class Constants {
+    public static final String BASE_URL = "https://api.nytimes.com";
+    public static final String API_KEY = "OKsEwghCzAPR3kRr7Hp51cFn2tMfXWgj";
+}
